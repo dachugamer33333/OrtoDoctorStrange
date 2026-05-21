2c110c5656b96937b99139388e3acbf0ed605f5b
dachu_dev <dachu_dev@MacBook-Air-de-dachu-dev.local>
2026-05-21 12:31:18 -0600

    vector verify

--- logica/vectores.py
+++ logica/vectores.py
@@ -1,18 +1,51 @@
 import numpy as np
+def proseso_ortogonal(conjunto_vectores):
+    n = len(conjunto_vectores)
+    for i in range(n):
+        for j in range(i + 1, n): 
+            producto = np.dot(conjunto_vectores[i], conjunto_vectores[j])
+           
+            if not np.isclose(producto, 0):  # isclose porque floats
+                return False
+    return True
+def proseso_ortonormal(conjunto_vectores,ortogonal):
+    if ortogonal:
+         for v in conjunto_vectores:
+            norma = np.linalg.norm(v)
+            if not np.isclose(norma, 1):
+                return False
+            return True
+    else:
+        return False
 
-def diagnostico_vectores():
-    vectores=[[1,90,1,2], [0,2,0,9], [0,0,1,0], [0,0,1,0]]
-    matriz = np.array(vectores)
-    print(matriz)
+    
+
+def diagnostico_vectores() -> None:
+    
+    conjunto_vectores=[[1,0,0], [1,1,0], [0,0,1]]
+    matriz = np.array(conjunto_vectores)
+    
     rango        = np.linalg.matrix_rank(matriz)
-    num_vectores = len(vectores)       # 3
-    dimension    = len(vectores[0])    # 4 ← aquí estaba el bug
+    
 
-    li  = rango == num_vectores        # 3 == 3 → True  ✓
-    gen = rango == dimension           # 3 == 4 → False ✓
-    base = li and gen                  # False  ✓
+    num_vectores = len(conjunto_vectores)       
+    dimension    = len(conjunto_vectores[0])    
 
+    li  = rango == num_vectores        
+    gen = rango == dimension           
+    base = li and gen                  
+    ortogonal=proseso_ortogonal(conjunto_vectores)
+    ortonormal=proseso_ortonormal(conjunto_vectores,ortogonal)
+    
+    
     print(f"Rango: {rango}")
     print(f"Lin. independiente: {li}")
     print(f"Generador: {gen}")
     print(f"Base: {base}")
+    if base:
+       print(f"Es Ortogonal: {ortogonal}")
+       print(f"Es ortonormal:{ortonormal}")
+       
+           
+           
+
